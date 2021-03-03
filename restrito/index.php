9b242691b23553be34961e5b36818b6e0c79dcf6
Caio <caiofilipee@gmail.com>
2021-03-03 17:10:21 -0300

    Criação do arquivo index.php
    Novo index do sistema.

---
+++ restrito/index.php
@@ -0,0 +1,52 @@
+<?php 
+  include "../validar.php";
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src='jquery-3.5.1.js'></script>
+    <script src='javascript.js'></script>
+    <link rel='stylesheet' href='stylesheet.css'>
+    
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <title>Cadastro</title>
+  </head>
+
+  <body>
+     <div class= "container">
+        <div class= "row">
+            <div class="col">
+                <div class="jumbotron">
+                    <h1 class="display-4">Bem vindo!</h1>
+                    <p class="lead">Fundação Podemos - Curso de Planejamento de Mandato</p>
+                    <hr class="my-4">
+                    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                    <p class="lead">
+                    <a class="btn btn-primary btn-lg" href="formulario.php" role="button">Formulário</a>
+                    <a class="btn btn-primary btn-lg" href="pesquisar.php" role="button">Pesquisar</a>
+                    <a class="btn btn-danger btn-lg" href="../logout.php" role="button">Sair</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+ 
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
